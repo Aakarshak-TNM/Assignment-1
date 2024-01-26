776ff859ca1170154d1c283cd5e500837d338737
Aakarshak-TNM <aakarshak.s@technmanconsulting.com>
2024-01-26 15:24:39 -0500

    List Task Done!

--- Java_Stream_Api/src/Opeartions.java
+++ Java_Stream_Api/src/Opeartions.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Opeartions {
@@ -13,30 +9,28 @@ public class Opeartions {
         // 2. Filter
         // 3. Sorted
 
-        // ArrayList<Integer> arr = new ArrayList<>();
-        // Collections.addAll(arr, 22, 33, 4, 5, 6, 7);
-        // System.out.println(arr);
+        ArrayList<Integer> arr = new ArrayList<>();
+        Collections.addAll(arr, 22, 33, 4, 5, 6, 7);
+        System.out.println(arr);
 
         // ----------------------------------------
         // Implement all methods of Stream API {GFG}
         // ----------------------------------------
 
-        // arr.stream().filter(element -> element % 2 ==
-        // 0).forEach(System.out::println);// Even Numbers
+        arr.stream().filter(element -> element % 2 == 0).forEach(System.out::println);// Even Numbers
 
         // Terminal Operations:-
         // 1. Reduce
         // 2. Collect
         // 3. For Each
-        // String result = arr.stream()
-        // .map(Object::toString) // Convert integers to strings
-        // .collect(Collectors.joining(", "));
-        // System.out.println(result);
+        String result = arr.stream()
+                .map(Object::toString) // Convert integers to strings
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
 
-        // int even = arr.stream().filter(x -> x % 2 == 0).reduce(0, (ans, i) -> ans +
-        // i);
-        // System.out.println(even);
+        int even = arr.stream().filter(x -> x % 2 == 0).reduce(0, (ans, i) -> ans +
+                i);
+        System.out.println(even);
 
-    
     }
 }
